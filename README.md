77e4810db95cc38041fd72d891e3376fb3a97aad
mercolino@gmail.com <mercolino@gmail.com>
2017-05-05 13:53:02 -0600

    Seed Host can be an ip address

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 Script to discover a network using CDP, it requires SQLite3 installed and paramiko library.
 
-**_Usage:_**
+**Usage:**
 cdp_discovery.py Seed_Host username password
 cdp_discovery.py report [tsv|txt]
 
-Seed_Host: Is the first host used to begin discover the network
-username and password: are the credential to connect to each device
+- **Seed_Host**: Is the first host used to begin discover the network
+- **username and password**: are the credential to connect to each device
 
 If you want to generate a report use the command report and you could choose between two formats:
-*txt*: Where just a list with the format hostname ip_address ssh is generated (Format taht can be used with my script Config Replicator), the only devices shown are switches, routers and WAP's
-*tsv*: Generate a list of all the devices found on the network and on a tab separated value that can be imported in excel.
 
-**_Note:_** This software is provided AS-IS and without any type of guarantee that will work or will discover all the devices on your network. If you use this software you agree that it is you responsbility and i do not have any kind of responsability if something happens with your network.
+- **txt**: Where just a list with the format hostname ip_address ssh is generated (Format taht can be used with my script Config Replicator), the only devices shown are switches, routers and WAP's
+
+- **tsv**: Generate a list of all the devices found on the network and on a tab separated value that can be imported in excel.
+
+**Note:** This software is provided AS-IS and without any type of guarantee that will work or will discover all the devices on your network. If you use this software you agree that it is you responsbility and i do not have any kind of responsability if something happens with your network.
--- cdp_discovery.py
+++ cdp_discovery.py
@@ -3,6 +3,7 @@ import paramiko
 import sqlite3
 import re
 import dns.resolver as dr
+import dns.reversename as rev
 
 
 class bcolors:
@@ -15,6 +16,9 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+class Error(Exception):
+    """Base Class for exceptions in this module"""
+    pass
 
 def recv_buffer(conn, stop_string):
     """
@@ -137,7 +141,7 @@ def ssh_connect(host, ip, db_conn):
 
 def insert_in_devices(conn2, info2):
     """
-    Fucntion to insert data to the database in the table devices
+    Function to insert data to the database in the table devices
     :param conn2: Database Connection
     :param info2: Dictionary with the devices gathered from processing the output of the sh cdp neighbor detail
     :return: NULL
@@ -163,6 +167,30 @@ def insert_in_devices(conn2, info2):
             print bcolors.OKBLUE + "Device {} already exists in the DB...".format(info2[device2]['hostname']) + bcolors.ENDC
 
 
+def ip_or_host(var):
+    """
+    Function to determine if a string is a hostname or an ip address
+    :param var: string to determine the type of address
+    :return: address_type: 'ip' for ip address and 'host' for host
+    """
+    pattern = '^([0-9]*)\.([0-9]*)\.([0-9]*)\.([0-9]*)$'
+    r = re.match(pattern, var)
+
+    if r:
+        if int(r.group(1)) > 0 and int(r.group(1)) <= 255 and \
+            int(r.group(2)) > 0 and int(r.group(2)) <= 255 and \
+            int(r.group(3)) > 0 and int(r.group(3)) <= 255 and \
+            int(r.group(4)) > 0 and int(r.group(4)) <= 255:
+
+            address_type = 'ip'
+        else:
+            raise Error('Wrong Format for an ip address!')
+    else:
+        address_type = 'host'
+
+    return address_type
+
+
 def help():
     """
     Function for HELP
@@ -173,90 +201,100 @@ def help():
     print bcolors.BOLD + "\tcdp_discovery report [tsv|txt]" +bcolors.ENDC
 
 
-# Check that the call has the right number of parameters
-if len(sys.argv) not in [3, 4]:
-    help()
-else:
-    # Connect to Devices
-    if sys.argv[1] != 'report':
-        # Connecting to the DB
-        print bcolors.HEADER + 'Connecting to the database...' + bcolors.ENDC
-        conn = sqlite3.connect('cdp.db')
-        c = conn.cursor()
-        # Drop the tables
-        print bcolors.HEADER + 'Droping tables...' + bcolors.ENDC
-        c.execute('DROP TABLE IF EXISTS devices')
-        conn.commit()
-        # Create the tables
-        print bcolors.HEADER + 'Creating tables...' + bcolors.ENDC
-        c.execute('CREATE TABLE devices (id INTEGER PRIMARY KEY, hostname, ip_address,\
-         platform, capabilities, status)')
-        # Connect and process the seed device
-        ip_from_host = dr.query(sys.argv[1])
-        ip_from_host = ip_from_host[0].address
-        output = ssh_connect(sys.argv[1], ip_from_host, conn)
-        if output != 'problem':
-            insert_in_devices(conn, output)
-        # Check there are still devices to connect to
-        c.execute("SELECT COUNT(*) FROM devices WHERE "
-                  "status = 0 AND "
-                  "(capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%' OR platform LIKE '%AIR%')")
-        no_devices = c.fetchone()[0]
-        # Connect to the rest of devices
-        while no_devices != 0:
-            # Select the device
-            c.execute("SELECT * FROM devices WHERE "
-                  "status = 0 AND "
-                  "(capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%' OR platform LIKE '%AIR%')"
-                  "LIMIT 1")
-            resp = c.fetchone()
-            hostname = resp[1]
-            ip_address = resp[2]
-            # Connect to device
-            output = ssh_connect(hostname, ip_address, conn)
-            if output != 'problem':
-                # Insert info on DB
-                insert_in_devices(conn, output)
-            # Check again for devices to connect on DB
-            c.execute("SELECT COUNT(*) FROM devices WHERE "
-                  "status = 0 AND "
-                  "(capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%' OR platform LIKE '%AIR%')")
-            no_devices = c.fetchone()[0]
-            print bcolors.HEADER + "#########################" + bcolors.ENDC
-            print bcolors.HEADER + "# Devices to connect... #" + bcolors.ENDC
-            print bcolors.HEADER + "#         {}            #".format(no_devices) + bcolors.ENDC
-            print bcolors.HEADER + "#########################" + bcolors.ENDC
-        conn.close()
+if __name__ == "__main__":
+    # Check that the call has the right number of parameters
+    if len(sys.argv) not in [3, 4]:
+        help()
     else:
-        # Generate Report
-        print bcolors.HEADER + "Generating Report..." + bcolors.ENDC
-        conn = sqlite3.connect('cdp.db')
-        c = conn.cursor()
-        # Text Format
-        if sys.argv[2] == 'txt':
-            f = open('report.txt', 'w')
-            try:
-                rows = c.execute("SELECT * FROM devices WHERE "
-                                 "capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%'")
-            except Exception as e:
-                print bcolors.FAIL + "Error: {}".format(e.message) + bcolors.ENDC
-                sys.exit(1)
-            for row in rows:
-                f.write(row[2] + ' ' + row[1] + ' ssh\n')
-            f.close()
-            print bcolors.OKGREEN + "Report generated Successfuly!!!" + bcolors.ENDC
-        # Generate report on Tab separated values format
-        elif sys.argv[2] == 'tsv':
-            f = open('report.tsv', 'w')
-            try:
-                rows = c.execute("SELECT * FROM devices")
-            except Exception as e:
-                print bcolors.FAIL + "Error: {}".format(e.message) + bcolors.ENDC
-                sys.exit(1)
-            for row in rows:
-                f.write(str(row[0]) + '\t' + row[1] + '\t' + row[2] + '\t' + row[3] + '\t' + row[4] + '\n')
-            f.close()
-            print bcolors.OKGREEN + "Report generated Successfuly!!!" + bcolors.ENDC
+        # Connect to Devices
+        if sys.argv[1] != 'report':
+            # Connecting to the DB
+            print bcolors.HEADER + 'Connecting to the database...' + bcolors.ENDC
+            conn = sqlite3.connect('cdp.db')
+            c = conn.cursor()
+            # Drop the tables
+            print bcolors.HEADER + 'Droping tables...' + bcolors.ENDC
+            c.execute('DROP TABLE IF EXISTS devices')
+            conn.commit()
+            # Create the tables
+            print bcolors.HEADER + 'Creating tables...' + bcolors.ENDC
+            c.execute('CREATE TABLE devices (id INTEGER PRIMARY KEY, hostname, ip_address,\
+             platform, capabilities, status)')
+            #Check if the seed value is an ip or a host
+            if ip_or_host(sys.argv[1]) == 'ip':
+                address = sys.argv[1]
+                host = dr.query(rev.from_address(sys.argv[1]), "PTR")[0].to_text()
+                host = host[:host.find('.')]
+            elif ip_or_host(sys.argv[1]) == 'host':
+                #If host query the name to return the ip address
+                ip_from_host = dr.query(sys.argv[1])
+                ip_from_host = ip_from_host[0].address
+                address = ip_from_host
+                host = sys.argv[1]
+            # Connect and process the seed device
+            output = ssh_connect(host, address, conn)
+            if output != 'problem':
+                insert_in_devices(conn, output)
+            # Check there are still devices to connect to
+            c.execute("SELECT COUNT(*) FROM devices WHERE "
+                      "status = 0 AND "
+                      "(capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%' OR platform LIKE '%AIR%')")
+            no_devices = c.fetchone()[0]
+            # Connect to the rest of devices
+            while no_devices != 0:
+                # Select the device
+                c.execute("SELECT * FROM devices WHERE "
+                      "status = 0 AND "
+                      "(capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%' OR platform LIKE '%AIR%')"
+                      "LIMIT 1")
+                resp = c.fetchone()
+                hostname = resp[1]
+                ip_address = resp[2]
+                # Connect to device
+                output = ssh_connect(hostname, ip_address, conn)
+                if output != 'problem':
+                    # Insert info on DB
+                    insert_in_devices(conn, output)
+                # Check again for devices to connect on DB
+                c.execute("SELECT COUNT(*) FROM devices WHERE "
+                      "status = 0 AND "
+                      "(capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%' OR platform LIKE '%AIR%')")
+                no_devices = c.fetchone()[0]
+                print bcolors.HEADER + "#########################" + bcolors.ENDC
+                print bcolors.HEADER + "# Devices to connect... #" + bcolors.ENDC
+                print bcolors.HEADER + "#         {}            #".format(no_devices) + bcolors.ENDC
+                print bcolors.HEADER + "#########################" + bcolors.ENDC
+            conn.close()
         else:
-            help()
-        conn.close()
+            # Generate Report
+            print bcolors.HEADER + "Generating Report..." + bcolors.ENDC
+            conn = sqlite3.connect('cdp.db')
+            c = conn.cursor()
+            # Text Format
+            if sys.argv[2] == 'txt':
+                f = open('report.txt', 'w')
+                try:
+                    rows = c.execute("SELECT * FROM devices WHERE "
+                                     "capabilities LIKE '%Router%' OR capabilities LIKE '%Switch%'")
+                except Exception as e:
+                    print bcolors.FAIL + "Error: {}".format(e.message) + bcolors.ENDC
+                    sys.exit(1)
+                for row in rows:
+                    f.write(row[2] + ' ' + row[1] + ' ssh\n')
+                f.close()
+                print bcolors.OKGREEN + "Report generated Successfuly!!!" + bcolors.ENDC
+            # Generate report on Tab separated values format
+            elif sys.argv[2] == 'tsv':
+                f = open('report.tsv', 'w')
+                try:
+                    rows = c.execute("SELECT * FROM devices")
+                except Exception as e:
+                    print bcolors.FAIL + "Error: {}".format(e.message) + bcolors.ENDC
+                    sys.exit(1)
+                for row in rows:
+                    f.write(str(row[0]) + '\t' + row[1] + '\t' + row[2] + '\t' + row[3] + '\t' + row[4] + '\n')
+                f.close()
+                print bcolors.OKGREEN + "Report generated Successfuly!!!" + bcolors.ENDC
+            else:
+                help()
+            conn.close()
